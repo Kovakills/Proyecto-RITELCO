ab06403f690b8124c1a4df326144ddbdb510cf3a
Kovakills <thekovakills@gmail.com>
2024-09-27 23:03:33 -0500

    R

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'mysql://root:vEAHSXYzWrtrWsDBClWGMPiFpjwqtIxB@junction.proxy.rlwy.net:25853/railway'
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:JQbUTkjuoQbwOczSAvEITdXJfRtJXrfs@junction.proxy.rlwy.net:47149/railway'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
