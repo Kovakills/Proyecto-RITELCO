351358577c09e4887ecd63b6ce8848bc13418f71
Kovakills <thekovakills@gmail.com>
2024-12-18 08:59:58 -0500

    r

--- config.py
+++ config.py
@@ -1,3 +1,6 @@
+import os
+
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'mysql://root:phGnhAgLjHMvcoBWMYHjSvGSCPaoFRhm@junction.proxy.rlwy.net:16734/railway'
+    BASE_DIR = os.path.abspath(os.path.dirname(__file__))  # Obtiene la ruta absoluta del directorio actual
+    SQLALCHEMY_DATABASE_URI = f"sqlite:///{os.path.join(BASE_DIR, 'database.db')}"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
