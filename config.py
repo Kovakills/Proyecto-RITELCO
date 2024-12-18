0686d915586a4616bef9ed35848cfe0f2d59f9c5
Kovakills <thekovakills@gmail.com>
2024-12-18 15:01:28 -0500

    R

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
 class Config:
-    BASE_DIR = os.path.abspath(os.path.dirname(__file__))  # Obtiene la ruta absoluta del directorio actual
+    BASE_DIR = os.path.abspath(os.path.dirname(__file__))
     SQLALCHEMY_DATABASE_URI = f"sqlite:///{os.path.join(BASE_DIR, 'database.db')}"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
